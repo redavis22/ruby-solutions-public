42886cc82a734e346e1314005884b5c12c37562e
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-10 10:30:50 -0800

    tic_tac_toe.rb: enforce can't move in same place

--- w1d2/tic_tac_toe.rb
+++ w1d2/tic_tac_toe.rb
@@ -1,6 +1,14 @@
 class TicTacToe
+  class IllegalMoveError < RuntimeError
+  end
+
   def initialize(player1, player2)
-    @players = [player1, player2]
+    @turn = :x
+    @players = {
+      :x => player1,
+      :o => player2
+    }
+
     @rows = [
       [nil, nil, nil],
       [nil, nil, nil],
@@ -9,8 +17,9 @@ class TicTacToe
   end
 
   def place_mark(pos, mark)
-    # does't enforce overriding a mark
     x, y = pos[0], pos[1]
+
+    raise IllegalMoveError.new("Space already taken") unless @rows[x][y].nil?
     @rows[x][y] = mark
 
     self
@@ -48,9 +57,10 @@ class TicTacToe
 
   def winner
     (rows + cols + diagonals).any? do |triple|
-      # check to see if the game is three in a row, then return what
-      # it's three of.
-      return triple[0] if [[:x] * 3, [:o] * 3].include?(triple)
+      if [[:x] * 3, [:o] * 3].include?(triple)
+        mark = triple[0]
+        return @players[mark]
+      end
     end
 
     nil
@@ -61,16 +71,20 @@ class TicTacToe
     @rows.each { |row| p row }
   end
 
+  def play_turn
+    current_player = @players[@turn]
+    current_player.move(self, @turn)
+
+    @turn = ((@turn == :x) ? :o : :x)
+  end
+
   def run
     until won?
       show
-
-      current_player = @players.shift
-      current_player.move(self)
-      @players << current_player
+      play_turn
     end
 
-    puts "The #{winner} won the game!"
+    puts "#{winner.name} won the game!"
   end
 end
 
@@ -79,13 +93,18 @@ class HumanPlayer
     @name = name
   end
 
-  def move(game)
+  def move(game, mark)
     while true
       puts "#{@name}: please select your space"
 
       x, y = gets.chomp.split(",").map(&:to_i)
       if [x, y].all? { |coord| (0..2).include?(coord) }
-        game.place_mark([x, y], :o)
+        begin
+          game.place_mark([x, y], mark)
+        rescue TicTacToe::IllegalMoveError
+          puts "Illegal move!"
+          next
+        end
         break
       else
         # repeat if input invalid
