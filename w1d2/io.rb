c0bd93932f18fe4ab51872eedabfb1b10a36f9d2
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-11 08:13:18 -0800

    io.rb: review

--- w1d2/io.rb
+++ w1d2/io.rb
@@ -1,15 +1,25 @@
 def guessing_game
   secret = rand(1..100)
 
+  guesses = 0
   while true
     puts "Guess my secret!"
     guess = gets.to_i
 
-    if guess == secret
+    case guess <=> secret
+    when -1
+      puts "Too low!"
+    when 0
       puts "My secret is found out!"
       break
+    when 1
+      puts "Too high!"
     end
+
+    guesses += 1
   end
+
+  puts "It took you #{guesses} guesses"
 end
 
 def shuffle_file(filename)
