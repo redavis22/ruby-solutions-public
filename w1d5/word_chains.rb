9f5b0e9e4a287520a2f07294c1b4742f5dcfa44c
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-12 11:01:41 -0800

    write word_chains.rb

---
+++ w1d5/word_chains.rb
@@ -0,0 +1,98 @@
+require 'set'
+
+=begin
+Man is born free, and everywhere he is in chains.
+=end
+class WordChainer
+  def self.adjacent_words(word, candidates)
+    # the variable `adjacent_words` *shadows* the method
+    # `adjacent_words`, which means that when we use `adjacent_words`
+    # in the method, it will access the variable and not call the
+    # method. This is common, because side-effect free methods are
+    # often named after the thing they return.
+    adjacent_words = Set.new
+
+    candidates.each do |candidate|
+      next if candidate.length != word.length
+
+      diffs = 0
+      candidate.split("").each_with_index do |letter, index|
+        diffs += 1 if (word[index] != letter)
+      end
+
+      adjacent_words << candidate if (diffs == 1)
+    end
+
+    adjacent_words
+  end
+
+  def initialize(dictionary)
+    @dictionary = dictionary
+  end
+
+  def build_chain(source, target)
+    return nil if source.length != target.length
+
+    @candidates = @dictionary.select { |word| word.length == source.length }
+    @candidates = Set.new(@candidates) - [source]
+
+    # words we've reached in the previous round, and need to grow from
+    @recent_words = Set.new([source])
+    # map each word to the word we found it from; `source` has no
+    # parent
+    @parent_words = { source => nil }
+
+    # keep looping until we find the target, or can't find any new
+    # words
+    until (@parent_words.has_key?(target)) || (@recent_words.empty?)
+      new_words, new_parent_words = find_new_words
+
+      # for debugging
+      p new_words
+
+      # new_words are the new recent_words
+      @recent_words = new_words
+      @parent_words.merge!(new_parent_words)
+
+      # filter candidates of new_words; we never need to return to a
+      # word that we've found previously.
+      @candidates -= new_words
+    end
+
+    build_path(target)
+  end
+
+  private
+  def find_new_words
+    # note how this method doesn't modify any instance variables; a
+    # method like this is easy to reason about, because it
+    # communicates with the rest of the class through its return
+    # value, which we can track easily
+    new_words = Set.new
+    new_parent_words = {}
+
+    # take each of the recent words and grow it
+    @recent_words.each do |word|
+      adjacent_words = WordChainer.adjacent_words(word, @candidates)
+
+      new_words += adjacent_words
+      adjacent_words.each do |adjacent_word|
+        new_parent_words[adjacent_word] = word
+      end
+    end
+
+    [new_words, new_parent_words]
+  end
+
+  def build_path(target)
+    return nil unless @parent_words.has_key?(target)
+
+    reversed_path = [target]
+    # will stop at `source`, which has `nil` parent
+    until @parent_words[reversed_path.last].nil?
+      reversed_path << @parent_words[reversed_path.last]
+    end
+
+    reversed_path.reverse
+  end
+end
