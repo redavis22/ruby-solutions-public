3537065b3a41093847ec3a31af336002dcfe80a3
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-20 13:42:12 -0800

    begin chess: start Board class

---
+++ w2d3/board.rb
@@ -0,0 +1,28 @@
+class Board
+  def initialize
+    @rows = Board.make_empty_grid
+  end
+
+  def self.make_empty_grid
+    Array.new(8) do
+      Array.new(8)
+    end
+  end
+
+  def piece_at(pos)
+    raise "invalid pos" unless valid_pos?(pos)
+
+    i, j = pos
+    @rows[i][j]
+  end
+
+  def valid_pos?(pos)
+    pos.all? do |coord|
+      (0...8).include?(coord)
+    end
+  end
+
+  def empty?(pos)
+    piece_at(pos).nil?
+  end
+end
