3a2415c003ff7ef7610a9f325cea5194897be5d7
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-20 16:37:31 -0800

    pawn.rb: add ability to move two spaces

--- w2d3/pieces/pawn.rb
+++ w2d3/pieces/pawn.rb
@@ -15,6 +15,8 @@ class Pawn < SteppingPiece
     # Board's internal representation would require a change here...
     forward_dir = (color == :white) ? -1 : 0
     move_diffs << [forward_dir, 0]
+    # can move two spaces the first time
+    move_diffs << [forward_dir * 2, 0] if at_start_row?
 
     # handle side attacks
     [[forward_dir, -1], [forward_dir, 1]].each do |pos|
@@ -27,5 +29,9 @@ class Pawn < SteppingPiece
     end
 
     move_diffs
+  end
+
+  def at_start_row?
+    pos[0] == ((color == :white) ? 6 : 1)
   end
 end
