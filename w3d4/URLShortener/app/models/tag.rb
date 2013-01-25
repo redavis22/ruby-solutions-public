5424d7d37fa6b3b0ab6ba6eabe35f91aad3f3828
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-25 08:10:24 -0800

    add tagging associations

--- w3d4/URLShortener/app/models/tag.rb
+++ w3d4/URLShortener/app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag < ActiveRecord::Base
   attr_accessible :name
 
   validates :name, :presence => true, :uniqueness => true
+
+  has_many :taggings
+  has_many :urls, :through => :taggings
 end
