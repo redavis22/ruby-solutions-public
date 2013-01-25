d4cef430b403102a573388052e803bf5e5e763f7
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-25 08:23:46 -0800

    Tag: track most popular links within a tag

--- w3d4/URLShortener/app/models/tag.rb
+++ w3d4/URLShortener/app/models/tag.rb
@@ -5,4 +5,18 @@ class Tag < ActiveRecord::Base
 
   has_many :taggings
   has_many :urls, :through => :taggings
+
+  def most_popular_urls
+    # this is the "real" way to do it
+    urls
+      .select("shortened_urls.*, COUNT(*) AS click_count")
+      .joins(:visits)
+      .group("shortened_urls.id")
+
+    # this is the way you knew how to do on Thu
+#    urls.sort_by { |url| url.visits.count }.take(5)
+
+    # Bonus: the slight difference here is that the joins way will
+    # drop urls with no visits (why?)
+  end
 end
