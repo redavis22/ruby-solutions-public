67f1dd4e0dbae9a7d3e2f1ca5fb6fd7f50ec3ce3
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-25 07:26:50 -0800

    reorder validations and associations

--- w3d4/URLShortener/app/models/user.rb
+++ w3d4/URLShortener/app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   [ :name,
     :email ].each { |field| attr_accessible field }
 
+  validates :name, :presence => true
+  validates :email, :uniqueness => true, :presence => true
+
   has_many :submitted_urls, :class_name => "ShortenedUrl", :foreign_key => "submitter_id"
   has_many :visits
   has_many :visited_urls, :class_name => "ShortenedUrl", :through => :visits, :source => :shortened_url
-
-  validates :name, :presence => true
-  validates :email, :uniqueness => true, :presence => true
 end
