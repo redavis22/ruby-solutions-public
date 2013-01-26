5a33577fa61fd5addab180d966317f8e21d519fe
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-25 22:02:54 -0800

    write Deck::all_cards

--- assessment02a/card.rb
+++ assessment02a/card.rb
@@ -66,3 +66,17 @@ class Card
     VALUE_STRINGS[value] + SUIT_STRINGS[suit]
   end
 end
+
+class Deck
+  def self.all_cards
+    all_cards = []
+
+    Card.suits.each do |suit|
+      Card.values.each do |value|
+        all_cards << Card.new(suit, value)
+      end
+    end
+
+    all_cards
+  end
+end
