f1ea9ab6d9b763df26a01f6bc83ce3cbfc9c8350
Ned Ruggeri <ruggeri@self-loop.com>
2013-01-26 10:12:26 -0800

    remove extraneous values from cards

--- assessment02a/lib/card.rb
+++ assessment02a/lib/card.rb
@@ -25,22 +25,6 @@ class Card
     :ace   => "A"
   }
 
-  VALUES = {
-    :deuce => 2,
-    :three => 3,
-    :four  => 4,
-    :five  => 5,
-    :six   => 6,
-    :seven => 7,
-    :eight => 8,
-    :nine  => 9,
-    :ten   => 10,
-    :jack  => 11,
-    :queen => 12,
-    :king  => 13,
-    :ace   => 14
-  }
-
   # Returns an array of all suits
   def self.suits
     SUIT_STRINGS.keys
