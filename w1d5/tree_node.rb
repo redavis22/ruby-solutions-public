c704b8e5ceff06444d789d998672b90768ab00d8
Ned Ruggeri <ruggeri@self-loop.com>
2013-03-15 15:28:24 -0700

    rewrite tree node

--- w1d5/tree_node.rb
+++ w1d5/tree_node.rb
@@ -1,49 +1,8 @@
-class TreeNode
-  attr_accessor :value
-  attr_reader :parent
-
-  def initialize(value = nil)
-    @value = value
-
-    @parent = parent
-    @children = [nil, nil]
-  end
-
-  def left_child
-    @children[0]
-  end
-
-  def right_child
-    @children[1]
-  end
-
-  def children
-    @children.dup
-  end
-
-  def left_child=(child)
-    set_child(child, 0)
-  end
-
-  def right_child=(child)
-    set_child(child, 1)
-  end
-
-  def set_child(new_child, position)
-    raise IllegalArgumentError.new("invalid position") unless (0...@children.count).include?(position)
-
-    old_child = @children[position]
-
-    old_child.parent = nil if old_child
-    new_child.parent = self if new_child
-
-    @children[position] = new_child
-  end
-
+module Searchable
   def dfs(target)
     return self if value == target
 
-    @children.each do |child|
+    children.each do |child|
       next if child.nil?
 
       result = child.dfs(target)
@@ -64,6 +23,86 @@ class TreeNode
     end
 
     nil
+  end
+end
+
+class BinaryTreeNode
+  include Searchable
+
+  attr_accessor :value
+  attr_reader :parent
+
+  def initialize(value = nil)
+    @value = value
+    @parent = nil
+    @children = [nil, nil]
+  end
+
+  def children
+    @children.reject { |child| child.nil? }
+  end
+
+  def left_child
+    @children[0]
+  end
+
+  def right_child
+    @children[1]
+  end
+
+  def left_child=(child)
+    set_child(child, 0)
+  end
+
+  def right_child=(child)
+    set_child(child, 1)
+  end
+
+  protected
+  attr_writer :parent
+
+  def set_child(new_child, position)
+    unless [0, 1].include?(position)
+      raise IllegalArgumentError.new("invalid position")
+    end
+
+    old_child = @children[position]
+
+    # don't need to do anything if they're equal
+    return if old_child == new_child
+
+    old_child.parent = nil if old_child
+    new_child.parent = self if new_child
+
+    @children[position] = new_child
+  end
+end
+
+class PolyTreeNode
+  include Searchable
+
+  attr_accessor :value
+  attr_reader :parent
+
+  def initialize(value = nil)
+    @value = value
+
+    @parent = parent
+    @children = []
+  end
+
+  def children
+    @children.dup
+  end
+
+  def add_child(new_child)
+    children << new_child
+    new_child.parent = self
+  end
+
+  def remove_child(child)
+    children.delete(child)
+    child.parent = nil
   end
 
   protected
